415cdcc1329068c5d926515d434da069dce9ca71
Lucasmanuelmz <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-03-21 20:35:55 +0200

    Adicionei icones no cabecalho da pagina

--- src/header/index.jsx
+++ src/header/index.jsx
@@ -3,7 +3,9 @@ export default function Header() {
     return (
         <header className="header">
             <nav className="nav">
-                <a href="#">cvapp</a>
+                <a className="icons" href="#">cvapp</a>
+                <a className="icons" href="https://github.com/Lucasmanuelmz"><img className="img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg" /></a>
+                <a className="icons" href="https://www.linkedin.com/in/lucas-manuel199988463/"><img className="img" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg" /></a>
             </nav>
         </header>
     )
