36301c7870de57913e35af5370b6a8723ecca798
Lucasmanuelmz <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-03-21 20:52:24 +0200

    Validei formulario da pagina de edicao

--- src/displayPage/index.jsx
+++ src/displayPage/index.jsx
@@ -39,47 +39,47 @@ export default function DisplayData({
                 <div className="content">
             <legend className="legend">Personal Data</legend>
             <label className="label" htmlFor="firstName">You Name:</label>
-            <input type="text" autoComplete="givenName" name="firstName" value={firstName} id="firstName" onChange={handleChange}
-                className="input" placeholder="First name" />
+            <input type="text" pattern="^[A-Z][a-zA-Z]+$" autoComplete="givenName" name="firstName" value={firstName} id="firstName" onChange={handleChange}
+                className="input" placeholder="First name" required/>
 
             <label className="label" htmlFor="lastName">Surname:</label>
-            <input type="text" name="lastName" value={lastName} onChange={handleChange}
-                className="input" id='lastName' placeholder="Surname" />
+            <input  pattern="^[A-Z][a-zA-Z]+$" type="text" name="lastName" value={lastName} onChange={handleChange}
+                className="input" id='lastName' placeholder="Surname" required/>
 
             <label className="label" htmlFor="email">Email:</label>
             <input type="email" name="email" value={email} onChange={handleChange}
-                className="input" id="email" placeholder="Email" />
+                className="input" id="email" placeholder="Email" required/>
 
             <label className="label" htmlFor="tel">Tel:</label>
-            <input type="tel" name="tel" value={tel} onChange={handleChange}
-            className="input" id="tel" placeholder="Tel:" />
+            <input pattern="^[0-9]+$" type="tel" minLength={9} maxLength={12} name="tel" value={tel} onChange={handleChange}
+            className="input" id="tel" placeholder="Tel:" required/>
                 
             <legend className="legend">Professional Experience</legend>
             <label className="label" htmlFor="company">Company:</label>
-            <input type="text" autoComplete="givenName" name="company" value={company}
-                id="company" onChange={handleChange} className="input" placeholder="Witch company" />
+            <input type="text"  pattern="^[A-Z][a-zA-Z]+$" autoComplete="givenName" name="company" value={company}
+                id="company" onChange={handleChange} className="input" placeholder="Witch company" required />
 
             <label className="label" htmlFor="position">Business position:</label>
-            <input type="text" name="position" value={position} onChange={handleChange}
+            <input type="text" name="position"  pattern="^[A-Z][a-zA-Z]+$" required value={position} onChange={handleChange}
                 className="input" id='position' placeholder="Business position" />
             
             <label className="label" htmlFor="description">Describe you responsability:</label>
             <textarea name="description" className="input" onChange={handleChange}
-                placeholder="Describe your responsibilities in this position" value={description}
+                placeholder="Describe your responsibilities in this position" required value={description}
                 id="description" cols="20" rows="8"></textarea>
 
             <label className="label" htmlFor="start">Start of work:</label>
             <input type="date" name="start" value={start} onChange={handleChange}
-                className="input" id="start" placeholder="Start of work" />
+                className="input" id="start" placeholder="Start of work" required />
 
             <label className="label" htmlFor="finish">Finish of work:</label>
             <input type="date" name="finish" value={finish} onChange={handleChange}
-                className="input" id="finish" placeholder="Finish of work" />
+                className="input" id="finish" placeholder="Finish of work" required/>
             
             <legend className="legend">Education</legend>
             <label className="label" htmlFor="educationLevel">Educational level:</label>
             <select className="select" onChange={handleChange} name="educationLevel"
-                id="educationLevel" value={educationLevel}>
+                id="educationLevel" value={educationLevel} required>
                 <option>Select level</option>
                 <option value='Elementary School'>Elementary School</option>
                 <option value='High School'>High School</option>
@@ -91,12 +91,12 @@ export default function DisplayData({
             <label className="label" htmlFor="school">School:</label>
             <input type="text" name="school" value={school}
                 onChange={handleChange} className="input" id='school'
-                placeholder="School" />
+                placeholder="School"  pattern="^[A-Z][a-zA-Z]+$" required/>
 
             <label className="label" htmlFor="graduationYear">Graduation year:</label>
             <input type="date" name="graduationYear" value={graduationYear}
                 onChange={handleChange} className="input" id="graduationYear"
-                placeholder="Graduation year" />
+                placeholder="Graduation year" required/>
             </div>): (
                 <div className="content">
                     <div className="container-name">
